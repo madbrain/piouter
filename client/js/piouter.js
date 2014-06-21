f0a36c2863080c02baffa099ef1b46b0c9963290
thebignet <thebignet@gmail.com>
2014-06-21 12:23:20 +0200

    feat(client): change current user with typeahead better ui refresh when user is changed or when follow list changed

--- client/js/piouter.js
+++ client/js/piouter.js
@@ -6,21 +6,30 @@ app.controller('TestCtrl', function ($scope, $resource, $log, userId) {
     
 	$scope.message = '';
 
-	var Piou = $resource('http://localhost:8080/piou/:userId', {userId:'@userId'});
-    $scope.pious = Piou.query({userId:userId});
-
 	var User = $resource('http://localhost:8080/user/:userId/:action/:actionId', {userId:'@userId',action:'@action',actionId:'@actionId'},{
         matching: {method:'GET',params:{action:'filter'},isArray:true},
         follow: {method:'PUT',params:{action:'follow'}},
         unfollow: {method:'DELETE',params:{action:'follow'}}
     });
-    $scope.user = User.get({userId:userId});
+
+	var Piou = $resource('http://localhost:8080/piou/:userId', {userId:'@userId'});
 
 	var Follower = $resource('http://localhost:8080/follower/:userId', {userId:'@userId'});
-    $scope.followers = Follower.query({userId:userId});
+
+	$scope.refreshPious = function(){
+        $scope.pious = Piou.query({userId:$scope.user.id});
+	};
+
+    $scope.selectUser = function(id){
+        $scope.user = User.get({userId:id});
+        $scope.pious = Piou.query({userId:id});
+        $scope.followers = Follower.query({userId:id});
+    };
+
+    $scope.selectUser(userId);
 
     $scope.send = function () {
-        var params = {userId:$scope.author,message:$scope.message,date:Date.now()};
+        var params = {userId:$scope.user.id,message:$scope.message,date:Date.now()};
         var monPiou = new Piou(params);
         monPiou.$save(function(ret, putResponseHeaders){
             if(ret.code==0){
@@ -32,20 +41,30 @@ app.controller('TestCtrl', function ($scope, $resource, $log, userId) {
     };
 
     $scope.getUsersMatching = function(val){
-        return User.matching({userId:userId,actionId:val}).$promise.then(function(res){
-            $log.info(res);
+        return User.matching({userId:$scope.user.id,actionId:val}).$promise.then(function(res){
             return res;
         });
     };
 
     $scope.follow = function(userToFollow){
-        User.follow({userId:userId,actionId:userToFollow.id},function(){
+        User.follow({userId:$scope.user.id,actionId:userToFollow.id},function(){
             $scope.user.following.push(userToFollow);
+            $scope.refreshPious();
+            $scope.userToFollow='';
         });
     };
 
     $scope.unfollow = function(userToUnfollow){
-        User.unfollow({userId:userId,actionId:userToUnfollow.id});
+        User.unfollow({userId:$scope.user.id,actionId:userToUnfollow.id},function(){
+            $scope.refreshPious();
+            var newFollowers = _.without($scope.user.following,userToUnfollow);
+            $scope.user.following=angular.copy(newFollowers);
+        });
+    };
+
+    $scope.changeUser = function(user){
+        $scope.selectUser(user.id);
+        $scope.newUser='';
     };
 
 });
